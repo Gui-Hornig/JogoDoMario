8d89584d86b7c060671455fbf2ec538d26d41c68
Gui-hornig <guihornig@gmail.com>
2024-02-21 15:44:59 -0300

    js

--- Jogo da Memoria/js/login.js
+++ Jogo da Memoria/js/login.js
@@ -1,13 +1,21 @@
-const input = document.querySelector('.login__input');
-const button = document.querySelector('.login__button');
+const input = document.querySelector(".login__input");
+const button = document.querySelector(".login__button");
+const form = document.querySelector(".login-form");
 
-
-const validateInput = ({target}) => {
+const validateInput = ({ target }) => {
   if (target.value.length > 3) {
-    button.removeAttribute('disabled');
-  } else {
-    button.setAttribute('disabled', '');
+    button.removeAttribute("disabled");
+    return;
   }
-}
+  button.setAttribute("disabled", "");
+};
 
-input.addEventListener('input', validateInput);
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+localStorage.setItem('player', input.value);
+window.location ='pages/game.html';
+};
+
+input.addEventListener("input", validateInput);
+form.addEventListener("submit", handleSubmit);
