c21fb74da433be8c68291a8c4bfd216a126536e5
Gui-hornig <guihornig@gmail.com>
2024-02-23 15:01:19 -0300

    Update game.js

--- Jogo da Memoria/js/game.js
+++ Jogo da Memoria/js/game.js
@@ -1,11 +1,14 @@
+cosnt grid = document.querySelector('.grid');
+
+
 const createCard = () => {
   const card = document.createElement('div');
   const front = document.createElement('div');
   const back= document.createElement('div');
 
-  const.className = 'card';
-  const.className = 'face front';
-  const.className = 'face back';
+  card.className = 'card';
+  front.className = 'face front';
+  back.className = 'face back';
 
   card.appendChild(front);
   card.appendChild(back);
