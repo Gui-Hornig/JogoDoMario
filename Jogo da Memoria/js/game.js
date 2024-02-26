d5ba8ebdbec305c9ae52937839167d11ea9be231
Gui-hornig <guihornig@gmail.com>
2024-02-26 18:27:46 -0300

    Update game.js

--- Jogo da Memoria/js/game.js
+++ Jogo da Memoria/js/game.js
@@ -43,3 +43,11 @@ const loadGame = () => {
   
 }
 loadGame()
+
+
+"Marca";"Nome Fantasia";"Data pedido";"Data Pagamento";"Data Estorno";"Data Libera��o";"Data Prevista Pgto";"Lan�amento";"Ref. Pedido";"Entrega";"Tipo";"Status";"Valor";"Parcela";"Meio Pgto";"Modelo Financeiro"
+"ACOM";"Galaxy Inform�tica ";"02/02/2024";"02/02/2024";;"08/02/2024";"22/02/2024";"Venda Marketplace";"02-1023999104";"201023999104001";"Venda";"Liberado";"123,21";;"a vista";"Semanal"
+"ACOM";"Galaxy Inform�tica ";"02/02/2024";"02/02/2024";;"08/02/2024";"22/02/2024";"Comiss�o cobrada sobre a venda";"02-1023999104";"201023999104001";"Comissao";"Liberado";"-20,38";;"a vista";"Semanal"
+"ACOM";"Galaxy Inform�tica ";"02/02/2024";"02/02/2024";;"08/02/2024";"22/02/2024";"Frete B2W Entrega";"02-1023999104";"201023999104001";"Frete_B2W_Entrega";"Liberado";"-0,00";;"a vista";"Semanal"
+"ACOM";"Galaxy Inform�tica ";"02/02/2024";"02/02/2024";;"08/02/2024";"22/02/2024";"Tarifa Adicional";"02-1023999104";"201023999104001";"Tarifa_Adicional";"Liberado";"-3,31";;"a vista";"Semanal"
+"ACOM";"Galaxy Inform�tica ";"02/02/2024";"02/02/2024";;"08/02/2024";"22/02/2024";"Participa��o de Frete";"02-1023999104";"201023999104001";"Participacao_frete";"Liberado";"-32,90";;"a vista";"Semanal"
